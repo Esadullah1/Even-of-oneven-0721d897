561484b8238490da2d191f580772d9d2596ea205
Esadullah Bagci <2083226@talnet.nl>
2020-06-22 10:50:54 +0200

    Uitwerking van de exercise

--- even_of_oneven.php
+++ even_of_oneven.php
@@ -3,9 +3,9 @@
 
         $line = readline("Vul een getal in: ");
         $rest = $line % 2;
-        if ($rest == 0) {
+if ($rest == 0) {
             echo  "Dit is een even getal";
-}elseif ($rest != 0) {
+        } elseif ($rest != 0) {
             echo "Dit is een oneven getal";
 }
 ?>
