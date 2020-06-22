ba0a5d6de11efc886e857a7cf167996cc1474db1
Esadullah Bagci <2083226@talnet.nl>
2020-06-22 10:52:36 +0200

    Uitwerking van de exercise

--- even_of_oneven.php
+++ even_of_oneven.php
@@ -5,7 +5,7 @@
         $rest = $line % 2;
 if ($rest == 0) {
             echo  "Dit is een even getal";
-        } elseif ($rest != 0) {
+}elseif ($rest != 0) {
             echo "Dit is een oneven getal";
-}
+        }
 ?>
